de8b035f58d93b214a0eabfea6f9f638c1b71ad6
Your full name <acooper@spe.org>
2012-02-23 17:23:19 -0600

    Changing over to select

--- wharfwhacker.py
+++ wharfwhacker.py
@@ -56,7 +56,7 @@ class WharfWhacker:
       responses, blank, exceptions = select.select(self.connection_sockets,[],self.connection_sockets,59)
       for response in responses:
         conn , addr = response.accept()
-        self.check_ports(response.getpeername()[0],response.getsockname()[1])
+        self.check_ports(conn.getpeername()[0],conn.getsockname()[1])
                
   def check_ports(self,ip_address,port): 
     # Logic hell that deals with the ports, and where they are at in the authentication sequence
